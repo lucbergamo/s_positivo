9dceb8d43dbba80046d63ee3e1714b5815d361f9
lucbergamo <59293195+lucbergamo@users.noreply.github.com>
2026-03-25 22:41:10 -0300

    Pagina gastos

--- gastos/urls.py
+++ gastos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import index
+from .views import index, gastos
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('gastos/', gastos, name='gastos'),
 ]
